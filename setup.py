a7492848edfccbaf693bc020b832d91c86b59347
Yixing Lao <yixing.lao@gmail.com>
2019-09-03 13:51:04 -0700

    include headers in wheel

--- setup.py
+++ setup.py
@@ -6,14 +6,10 @@ setup(
     description=[
         "Quick fix for using Azure Kinect SDK with Open3D on Ubuntu 16.04"
     ],
-    # cmdclass=cmdclass,
     include_package_data=True,
     license="MIT",
-    # Name of the package on PyPI
-    name="open3d_azure_kinect_ubuntu1604_fix",
-    packages=[
-        'open3d_azure_kinect_ubuntu1604_fix',
-    ],
+    name="open3d_azure_kinect_ubuntu1604_fix", # On PyPI
+    packages=['open3d_azure_kinect_ubuntu1604_fix'], # Actual package
     url="https://github.com/yxlao/open3d_azure_kinect_ubuntu1604_fix",
     project_urls={
         'Source code': 'https://github.com/yxlao/open3d_azure_kinect_ubuntu1604_fix',
